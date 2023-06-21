ade5822eb8cbde5e863a4439e4e7690d744bd203
muhamadsahrulll <sahrulsmp4@gmail.com>
2023-06-21 20:43:07 +0700

    pertama

---
+++ lib/camera.dart
@@ -0,0 +1,77 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+typedef void Callback(List<dynamic> list);
+
+class Camera extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  final Callback setRecognitions;
+
+  Camera(this.cameras, this.setRecognitions);
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  late CameraController cameraController;
+  late bool isDetecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    cameraController =
+        CameraController(widget.cameras.first, ResolutionPreset.high);
+    cameraController.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+
+      cameraController.startImageStream((image) {
+        if (!isDetecting) {
+          isDetecting = true;
+          Tflite.runModelOnFrame(
+            bytesList: image.planes.map((plane) {
+              return plane.bytes;
+            }).toList(),
+            imageHeight: image.height,
+            imageWidth: image.width,
+            imageStd: 127.5,
+            rotation: 90,
+            threshold: 0.1,
+            numResults: 1,
+          ).then((value) {
+            if (value!.isNotEmpty) {
+              widget.setRecognitions(value);
+              isDetecting = false;
+            }
+          });
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!cameraController.value.isInitialized) {
+      return Container();
+    }
+
+    return Transform.scale(
+      scale: 5 / cameraController.value.aspectRatio,
+      child: Center(
+        child: AspectRatio(
+          aspectRatio: cameraController.value.aspectRatio,
+          child: CameraPreview(cameraController),
+        ),
+      ),
+    );
+  }
+}
